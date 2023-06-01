aace6516b131dda8204ca631548b3eaa9c437249
Ryan Trimble <mrtrimble2@gmail.com>
2023-06-01 13:18:06 -0400

    first commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <FormKit type="text"
+      name="first-name"
+      id="first-name"
+      label="First Name"
+      placeholder="“Jon”"
+    />
+    <FormKit type="text"
+      name="last-name"
+      id="last-name"
+      label="Last Name"
+      placeholder="“Doe”"
+    />
+  </form>
+</template>
